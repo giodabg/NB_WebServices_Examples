997e0335ef66186eebc4deed415037daada235bf
giodabg <1148340+giodabg@users.noreply.github.com>
2020-03-16 08:48:50 +0100

    Esempio utilizzo web service con Consumer JAVA GET parametrizzata

--- NBWSConsumerGet/src/nbwsconsumerget/NBWSConsumerGet.java
+++ NBWSConsumerGet/src/nbwsconsumerget/NBWSConsumerGet.java
@@ -3,25 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package nbwsconsumerget;
+package nb_https_getlower;
+
+import java.io.UnsupportedEncodingException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author scuola
+ * @author Prof
  */
-public class NBWSConsumerGet {
+public class NB_HTTPS_GETlower {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        WSConsumer webService = new WSConsumer("https://www.gerriquez.com/comuni/ws.php?");
-        
-        webService.get("dencomune", "Mariano Comense");
-        webService.printResult();
-        
-        webService.get("regione", "lombardia");
-        webService.printResult();
-    }
-    
+
+            WSConsumer webService;
+            // webService = new WSConsumer("https://www.gerriquez.com/comuni/ws.php?");
+            
+            // webService.AddParameter("dencomune", "Mariano Comense");
+            // webService.get("www.gerriquez.com");
+            // webService.printResult();
+            
+            // webService.AddParameter("regione", "lombardia");
+            // webService.get("www.gerriquez.com");
+            // webService.printResult();
+            
+            webService = new WSConsumer("https://www.ismonnet.gov.it/public/circolari/Circolari_3764B_2019-2020.pdf");
+            webService.get("www.ismonnet.gov.it");
+            webService.printResult();
+            
+        // try {
+            // webService = new WSConsumer("https://it.wikipedia.org/w/index.php");
+            // webService.AddParameter("title", "Client-server");
+            // webService.get("it.wikipedia.org");        
+            // webService.printResult();
+        // } catch (UnsupportedEncodingException ex) {
+        //    Logger.getLogger(NB_HTTPS_GETlower.class.getName()).log(Level.SEVERE, null, ex);
+        // }
+        }
 }
